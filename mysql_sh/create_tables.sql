02776943e8b7c7196b789da0b27c0b45acdc239b
root <root@mysql.novalocal>
2016-07-13 09:41:37 +0800

    Add mysql insert shell

---
+++ mysql_sh/create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `unit` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
---
+++ mysql_sh/insert.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import thread
+import MySQLdb
+import uuid 
+import time
+import random
+
+
+data = ['cpu_detil','mem_used','disk_io','net_id']
+servers = ['server60','server61','server62','server63','server64']
+
+def insert(uuid=None,name=None, data_type=None, unit=None, time=None):
+    try:
+        conn=MySQLdb.connect(host='localhost',user='root',passwd='',db='gnocchi',port=3306)
+        cur=conn.cursor()
+        cur.execute("insert into test values(%s,%s,%s,%s,%s)", [uuid,name,data_type,unit,time])
+	conn.commit()
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+         print "Mysql Error"
+         print e 
+
+
+def insert_many():
+    while 1:
+      insert(str(uuid.uuid4()), 
+		servers[random.randint(0,3)], 
+		data[random.randint(0,3)], 
+		random.randint(1,100), 
+		time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+	    )
+      time.sleep(0.1)
+
+
+def create_thread():
+    thread.start_new_thread(insert_many, ())
+
+for i in range(1,8):
+    create_thread()
+
+# sleep 10 h
+time.sleep(60*60*10)
